745a6c9eca0eb6534e9cd71d3a91a75783fccffe
aml <aml@simontech.es>
2020-07-09 12:11:07 +0200

    Feedback going up & going down prioritary over excluded state

--- main/BLINDS_Feedback.c
+++ main/BLINDS_Feedback.c
@@ -63,32 +63,22 @@ void _feedback_signal_Task(void * xParams)
         MTX_Lock(&xFeedbackMtx);
         if (TMR_GetPollTimeRunning(&xTimerSignal) == true) {       // Wait here while timer is running
             TMR_GetPollTimeElapsed(&xTimerSignal);
-        } else if (MEM_GetStatus() == MEM_STATUS_HIDE) {
-            LED_Off(&xLedUp, false);
-            LED_Blink(&xLedDown, SIGNAL_OFFLINE_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_OFFLINE_ON, SIGNAL_OFFLINE_OFF, false);
         } else if (LOAD_IsCalibrating() == true) {
             LED_Blink(&xLedUp, SIGNAL_CALIBRATE_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_CALIBRATE_ON, SIGNAL_CALIBRATE_OFF, false);
             LED_Blink(&xLedDown, SIGNAL_CALIBRATE_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_CALIBRATE_ON, SIGNAL_CALIBRATE_OFF, false);
+        } else if (LOAD_IsGoingUp() == true) {
+            LED_Off(&xLedDown, false);
+            LED_Blink(&xLedUp, SIGNAL_BLIND_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_BLIND_ON, SIGNAL_BLIND_OFF, false);
+        } else if (LOAD_IsGoingDown() == true) {
+            LED_Off(&xLedUp, false);
+            LED_Blink(&xLedDown, SIGNAL_BLIND_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_BLIND_ON, SIGNAL_BLIND_OFF, false);
+        } else if (MEM_GetStatus() == MEM_STATUS_HIDE) {
+            LED_Off(&xLedUp, false);
+            LED_Blink(&xLedDown, SIGNAL_OFFLINE_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_OFFLINE_ON, SIGNAL_OFFLINE_OFF, false);
         } else if (xIdleSignal == FEEDBACK_IDLE_ON) {
-            if (LOAD_IsGoingUp() == true) {
-                LED_Off(&xLedDown, false);
-                LED_Blink(&xLedUp, SIGNAL_BLIND_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_BLIND_ON, SIGNAL_BLIND_OFF, false);
-            } else if (LOAD_IsGoingDown() == true) {
-                LED_Off(&xLedUp, false);
-                LED_Blink(&xLedDown, SIGNAL_BLIND_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_BLIND_ON, SIGNAL_BLIND_OFF, false);
-            } else {
-                LED_On(&xLedUp, 20, false); LED_On(&xLedDown, 20, false);
-            }
+            LED_On(&xLedUp, 20, false); LED_On(&xLedDown, 20, false);
         } else if (xIdleSignal == FEEDBACK_IDLE_OFF) {
-            if (LOAD_IsGoingUp() == true) {
-                LED_Off(&xLedDown, false);
-                LED_Blink(&xLedUp, SIGNAL_BLIND_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_BLIND_ON, SIGNAL_BLIND_OFF, false);
-            } else if (LOAD_IsGoingDown() == true) {
-                LED_Off(&xLedUp, false);
-                LED_Blink(&xLedDown, SIGNAL_BLIND_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_BLIND_ON, SIGNAL_BLIND_OFF, false);
-            } else {
-                LED_Off(&xLedUp, false); LED_Off(&xLedDown, false);
-            }
+            LED_Off(&xLedUp, false); LED_Off(&xLedDown, false);
         } else {
             LED_Off(&xLedUp, false);
             LED_Blink(&xLedDown, SIGNAL_ERROR_LEVEL/100, LED_BLINK_ALWAYS, SIGNAL_ERROR_ON, SIGNAL_ERROR_OFF, false);
