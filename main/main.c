2c4c7f5a6e2d59cdaa380911269037327e096286
aml <aml@simontech.es>
2021-02-24 19:28:20 +0100

    version 2.0.2

--- main/main.c
+++ main/main.c
@@ -74,7 +74,7 @@ esp_chip_info_t chip_info;
     printf("********************************\n");
     fflush(stdout);
 
-    NVS_Init(); NVS_WriteStrToModule("OTA", "FW", "21000333_D_2.0.1");
+    NVS_Init(); NVS_WriteStrToModule("OTA", "FW", "21000333_D_2.0.2");
 
     if(TEST_IsFactoryTestPassed() == true) {
         if (MEM_StartComs(0, DEVICE_SetCmd, DEVICE_GetCmd, NULL, DEVICE_Reset) == false) ESP_LOGE(TAG_MAIN, "Error creating MEM Wireless coms\n");
